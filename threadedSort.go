fa8dac46dd984568998c85082855ca7442bf789c
Usama <usama24march@gmail.com>
2019-07-31 23:34:29 +0500

    added delay coefficient

--- threadedSort.go
+++ threadedSort.go
@@ -5,6 +5,8 @@ import (
     "time"
     "math/rand"
     "sync"
+    "sort"
+    "math"
 )
 
  func delayMilliSec(yourTime int){
@@ -17,11 +19,12 @@ import (
 	var mutex = &sync.Mutex{}
 	index:= 0
 	sortedArray:= make([]int,len(array))
+	delayCoeff:= estimateDelayCoeff(len(array))
  	for i := 0; i < len(array); i++ {
  		go func(){
  			currNum:=array[i]
  			<-startInsideLoop
- 			delayMilliSec(currNum*10)
+ 			delayMilliSec(currNum*delayCoeff)
  			mutex.Lock()
  			sortedArray[index]= currNum
  			index++
@@ -45,9 +48,24 @@ import (
 	return returnArray
  }
 
+ func estimateDelayCoeff(n int)(int){
+ 	exp:= 4.5503685360*(math.Pow(1.0004632701,float64(n)))
+ 	return int(math.Ceil(exp))
+ }
+
+ func isSorted(original []int, sorted []int)(bool){
+ 	sort.Ints(original)
+ 	for i:= 0;i<len(original);i++{
+ 		if(original[i]!=sorted[i]){
+ 			return false
+ 		}
+ 	}
+ 	return true
+ }
 func main(){
-	array:= makeRandomArray(300,10)
-	fmt.Println("Random Array:",array)
+	array:= makeRandomArray(3000,100)
+	// fmt.Println("Random Array:",array)
 	sorted:= threadedSort(array)
-	fmt.Println("Sorted Array:",sorted)
+	// fmt.Println("Sorted Array:",sorted)
+	fmt.Println(isSorted(array, sorted))
 }
